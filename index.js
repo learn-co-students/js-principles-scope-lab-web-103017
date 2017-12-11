952c249401acd4eb9da00927f565f21986b0850d
dansbands <dansbands@hotmail.com>
2017-12-10 22:18:46 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+const leastFavoriteCustomer = 'matt'
+
+function upperCaseCustomerName () {
+  return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob'
+}
+
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'alex'
+}
+
+function attemptTwoFavoriteCustomers() {
+  const favoriteCustomer = 'billy'
+  let favoriteCustomer = 'bobby'
+}
