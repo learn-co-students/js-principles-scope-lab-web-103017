cea0730928630718eb46172285c80ca1f0e89c10
lierica <ericaaa.li@gmail.com>
2017-12-10 21:35:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+	customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+	bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+	bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "erica";
+
+function changeLeastFavoriteCustomer() {
+	leastFavoriteCustomer = "not erica";
+}
+
+function attemptTwoFavoriteCustomers() {
+	const favoriteCustomer = "tim"
+ 	let favoriteCustomer = "meryl"
+}
