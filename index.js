20039e817e9540a8fcf7288bf7dfe10b84444121
RochelLevi <rochlevi@gmail.com>
2017-12-11 07:59:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "rochel"
+
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "soroh"
+}
+
+function attemptTwoFavoriteCustomers(){
+  let favoriteCustomer = 'adam';
+  let favoriteCustomer = 'rochel';
+}
