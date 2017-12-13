92e4791169cefb0832675095d52c96e0d7ef3caf
Josh-Stillman <joshstillman@gmail.com>
2017-12-13 11:32:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = "bob"
+const leastFavoriteCustomer = "bill"
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+  bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = "maybe bob"
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = "bob"
+}
+
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = "big sneil"
+  let favoriteCustomer = "sneil"
+}
